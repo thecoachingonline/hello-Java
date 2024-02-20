e885e2d6db8f7ea92ae64961627229f91c476d4e
Y.Nanthachai <thecoachingonline@gmail.com>
2024-02-20 09:38:53 +0000

    Hi!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld { //กำหนดคลาสชื่อ HelloWorld
    public static void main(String[] args) { //กำหนด Method คือ Main และสร้าง Object ที่เป็น String ขึ้นมา (String ก็คือ Types Char ที่เรียบเรียงลำดันบกัน)
-      System.out.println("Hello, world!"); //กำหนด Method ชื่อ PrintIn ให้พิมพ์ Object ทางจอภาพแล้วกำหนดค่า String เป็น "HelloWorld"
+      System.out.println("สวัสดี, ชาวโลก!"); //กำหนด Method ชื่อ PrintIn ให้พิมพ์ Object ทางจอภาพแล้วกำหนดค่า String เป็น "HelloWorld"
    }
 }
--- JavaApplet/TestApp.java
+++
@@ -1,8 +0,0 @@
-import java.lang.*;
-
-class TestApp {
-
-    public static void main(String[] args) {
-        System.out.println("My Java installation work...");
-    }
-}
